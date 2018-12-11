6a7f1218954c640f1e8f623e1530439209fc4447
Unknown <st.aska.29.07@gmail.com>
2018-12-11 21:06:56 +0300

    Added sort to highscores.

--- highscore-service/src/main/java/com/anastaciamath/game/controller/HighscoreController.java
+++ highscore-service/src/main/java/com/anastaciamath/game/controller/HighscoreController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
@@ -29,6 +30,7 @@ public class HighscoreController {
             uhdto.setScore(score.getScore());
             scores.add(uhdto);
         });
+        scores.sort(Comparator.comparingInt(UserHighscoreDTO::getScore));
         return scores;
     }
 
